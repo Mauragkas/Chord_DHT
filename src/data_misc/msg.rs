5f1d6d224cbd77c6f635f12156e09a212738fed6
Mauragkas <stathpan03@gmail.com>
2025-01-02 15:13:57 +0200

    added messages

--- src/data_misc/msg.rs
+++ src/data_misc/msg.rs
@@ -1,26 +1,66 @@
 use super::*;
 use data::*;
-use node_state::*;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum Message {
-    IAmYourSuccessor { node_id: String },
-    IAmYourPredecessor { node_id: String },
-    ReqKnownNode { node_id: String },
-    ResKnownNode { node_id: String },
-    Data { from: String, data: Vec<Data> },
-    UpdateNodeState { new_state: NodeState },
-    ReqJoin { node_id: String },
-    ResJoin { node_id: String, sender_id: String },
+    IAmYourSuccessor {
+        node_id: String,
+    },
+    IAmYourPredecessor {
+        node_id: String,
+    },
+    ReqKnownNode {
+        node_id: String,
+    },
+    ResKnownNode {
+        node_id: String,
+    },
+    Data {
+        from: String,
+        data: Vec<Data>,
+    },
+    ReqJoin {
+        node_id: String,
+    },
+    ResJoin {
+        node_id: String,
+        sender_id: String,
+    },
     RingIsFull,
-    Success { message: String },
-    ErrorMessage { error: String },
-    Lookup { key: String },
+    Success {
+        message: String,
+    },
+    ErrorMessage {
+        error: String,
+    },
+    LookupReq {
+        key: String,
+        hops: usize,
+    },
+    LookupRes {
+        key: String,
+        hops: usize,
+        data: Option<Vec<Data>>,
+    },
     NodeExists,
-    Notify { node_id: String },
-    Leave { node_id: String },
-    ReqFinger { from: String, index: usize },
-    ResFinger { node_id: String, index: usize },
+    Notify {
+        node_id: String,
+    },
+    Leave {
+        node_id: String,
+    },
+    ReqFinger {
+        from: String,
+        index: usize,
+    },
+    ResFinger {
+        node_id: String,
+        index: usize,
+    },
+    CheckNode {
+        node_id: String,
+    },
     Ping,
     Pong,
+    Kys,
 }
