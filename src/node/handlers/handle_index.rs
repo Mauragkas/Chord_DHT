bde319584b942a8324830f689467a19e31b85203
Mauragkas <stathpan03@gmail.com>
2025-01-02 15:19:52 +0200

    minimal changes and features

--- src/node/handlers/handle_index.rs
+++ src/node/handlers/handle_index.rs
@@ -1,9 +1,13 @@
 use super::*;
+use crate::node::node::CHORD_RING; // Add this import at the top
 pub async fn handle_index(data: web::Data<Node>) -> impl Responder {
     let node_state = data.node_state.lock().await;
     let logs = data.logs.lock().await;
     let conn = data.db.lock().await;
-    let mut stmt = conn.prepare("SELECT * FROM data").unwrap();
+    let mut stmt = conn
+        .prepare("SELECT * FROM data ORDER BY hash ASC")
+        .unwrap();
+
     let data_iter = stmt
         .query_map([], |row| {
             Ok((
@@ -26,7 +30,7 @@ pub async fn handle_index(data: web::Data<Node>) -> impl Responder {
         "{{node_id}}",
         format!("{} [{}]", node_state.id, hash(&node_state.id)).as_str(),
     );
-    html = html.replace("{HOME_URL}", &format!("http://{}:{}", *IP, *PORT));
+    html = html.replace("{HOME_URL}", &format!("http://{}", CHORD_RING.lock().await));
     html = html.replace(
         "{{predecessor}}",
         format!(
--- src/node/handlers/handle_ins.rs
+++ src/node/handlers/handle_ins.rs
@@ -1,7 +1,9 @@
 use super::*;
-pub async fn handle_insert(data: web::Data<Node>, data_to_ins: web::Json<Data>) -> impl Responder {
+pub async fn handle_insert(
+    data: web::Data<Node>,
+    data_to_ins: web::Json<Vec<Data>>,
+) -> impl Responder {
     let node_state = data.node_state.lock().await;
-    let data_hash = hash(&data_to_ins.key);
     let node_hash = hash(&node_state.id);
     let prev_hash = node_state
         .predecessor
@@ -9,18 +11,30 @@ pub async fn handle_insert(data: web::Data<Node>, data_to_ins: web::Json<Data>) 
         .map(|id| hash(id))
         .unwrap_or(node_hash);
 
-    log_message!(data, "Handling insert request for key: {}", data_to_ins.key);
-    log_message!(
-        data,
-        "Hash values - Data: [{}], Node: [{}], Predecessor: [{}]",
-        data_hash,
-        node_hash,
-        prev_hash
-    );
+    let mut local_data = Vec::new();
+    let mut forward_data = Vec::new();
 
-    if is_between(prev_hash, data_hash, node_hash) {
+    for item in data_to_ins.iter() {
+        let data_hash = hash(&item.key);
+        log_message!(data, "Handling insert request for key: {}", item.key);
+        log_message!(
+            data,
+            "Hash values - Data: [{}], Node: [{}], Predecessor: [{}]",
+            data_hash,
+            node_hash,
+            prev_hash
+        );
+
+        if is_between(prev_hash, data_hash, node_hash) {
+            local_data.push(item.clone());
+        } else {
+            forward_data.push(item.clone());
+        }
+    }
+
+    if !local_data.is_empty() {
         log_message!(data, "Data belongs to this node, inserting locally");
-        match data.insert_data(vec![data_to_ins.into_inner()]).await {
+        match data.insert_batch_data(local_data).await {
             Ok(_) => {
                 log_message!(data, "Data inserted successfully");
                 HttpResponse::Ok().body("Data inserted successfully")
@@ -30,19 +44,18 @@ pub async fn handle_insert(data: web::Data<Node>, data_to_ins: web::Json<Data>) 
                 HttpResponse::InternalServerError().body(err.to_string())
             }
         }
-    } else {
+    } else if !forward_data.is_empty() {
         // Forward to successor if data doesn't belong here
         if let Some(successor) = &node_state.successor {
             log_message!(data, "Forwarding data to successor node: {}", successor);
-            send_post_request!(
-                &format!("http://{}/insert", successor),
-                data_to_ins.into_inner()
-            );
+            send_post_request!(&format!("http://{}/insert", successor), forward_data);
             log_message!(data, "Data forwarded successfully to successor");
             HttpResponse::Ok().body("Data forwarded to successor")
         } else {
             log_message!(data, "No successor found, cannot forward data");
             HttpResponse::InternalServerError().body("No successor found")
         }
+    } else {
+        HttpResponse::Ok().body("No data to process")
     }
 }
